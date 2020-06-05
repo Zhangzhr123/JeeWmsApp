5e60dd2a014b48eda142fab853f465e88716f505
张忠睿 <zhangzhr@hichane.cn>
2020-06-05 13:50:52 +0800

    修改BUG

--- app/src/main/java/com/jeewms/www/wms/ui/itemview/HomeGridItemView.java
+++ app/src/main/java/com/jeewms/www/wms/ui/itemview/HomeGridItemView.java
@@ -60,16 +60,16 @@ public class HomeGridItemView {
         else if(Constance.btnNameList[1].equals(btnName)){
             TLReceiptActivity.show(mActivity);
         }
-//        else if(Constance.btnNameList[2].equals(btnName)){
-//            //按货捡单
-//            PickingDetailActivity.show(mActivity);
-//        }else if(Constance.btnNameList[3].equals(btnName)){
-//            RecheckLoadingActivity.show(mActivity);
-//        }else if(Constance.btnNameList[4].equals(btnName)){
-//            StorageMoveActivity.show(mActivity);
-//        }else if(Constance.btnNameList[5].equals(btnName)){
-//            InventoryActivity.show(mActivity);
-//        }
+        else if(Constance.btnNameList[2].equals(btnName)){
+            //按货捡单
+            PickingDetailActivity.show(mActivity);
+        }else if(Constance.btnNameList[3].equals(btnName)){
+            RecheckLoadingActivity.show(mActivity);
+        }else if(Constance.btnNameList[4].equals(btnName)){
+            StorageMoveActivity.show(mActivity);
+        }else if(Constance.btnNameList[5].equals(btnName)){
+            InventoryActivity.show(mActivity);
+        }
 //        else if(Constance.btnNameList[6].equals(btnName)){
 //            GoodsInfoActivity.show(mActivity);
 //        }else if(Constance.btnNameList[7].equals(btnName)){
